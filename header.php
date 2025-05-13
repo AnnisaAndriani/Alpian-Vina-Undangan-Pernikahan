dca651cd1471ac16f17b26d60eb67e8915244046
Annisa Andriani <annisandriani17@gmail.com>
2025-05-13 09:59:49 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Annisa Andriani</title>
+    <link rel="icon" type="image/ico" href="/assets/icon.ico">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <style>
+    @font-face {
+        font-family: 'Plus Jakarta Sans';
+        src: url('assets/fonts/PlusJakartaSans-VariableFont_wght.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    body {
+        background-color: #fff;
+        font-family: 'Plus Jakarta Sans', sans-serif;
+    }
+
+    .navbar-custom {
+        background-color: #F7CAC9;
+        border-radius: 20px;
+        padding: 10px;
+    }
+
+    .nav-link {
+        color: #fff;
+        font-weight: bold;
+        padding: 0 20px;
+    }
+
+    .nav-link:hover {
+        color: #4335A7;
+    }
+
+    .nav-active {
+        color: #91A8D0;
+    }
+
+    ::-webkit-scrollbar {
+        width: 12px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: #fff;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: #555;
+        border-radius: 10px;
+        border: 3px solid #fff;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: #4335A7;
+    }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-custom justify-content-center rounded-5 fixed-top mt-2 w-75 mx-auto">
+        <button class="navbar-toggler navbar-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link nav-active" href="#beranda">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#tentangsaya">Tentang Saya</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#facilities">Facility</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contactme">Contact Me</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/riwayat">Riwayat Booking</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
